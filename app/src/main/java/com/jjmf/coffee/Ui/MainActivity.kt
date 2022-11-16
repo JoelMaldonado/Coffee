499514991103fce27ea2c88e17cb3fbdf9d3681c
joelj <joelmaldonadodev@gmail.com>
2022-11-15 21:52:34 -0500

    Clase 05

--- app/src/main/java/com/jjmf/coffee/Ui/MainActivity.kt
+++ app/src/main/java/com/jjmf/coffee/Ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jjmf.coffee.Ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jjmf.coffee.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,9 +10,12 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        screenSplash.setKeepOnScreenCondition{false}
 
 
         events()
